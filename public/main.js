81d8572445fa93b2ee72cb5f3d8a48f3697f7b9e
fnspecter69 <nfreddy49@gmail.com>
2020-06-23 21:47:00 -0400

    fix compile folder

--- public/main.js
+++
@@ -1,1 +0,0 @@
-console.error("Error: Plugin/Preset files are not allowed to export objects, only functions. In /home/fwn11/MyProjects/webproject/react-skeleton/node_modules/babel-preset-react/lib/index.js while parsing file: /home/fwn11/MyProjects/webproject/react-skeleton/src/main.jsx");
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
-var ingredients = [{"id":1, "test":"ham"}, {"id":2, "text", "cheese"}, {"id":3, "text": "potatoes"}]
+var ingredients = [{"id":1, "test":"ham"}, {"id":2, "text":"cheese"}, {"id":3, "text":"potatoes"}]
 
 var List = React.createClass({
   render: function() {
